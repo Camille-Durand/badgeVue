31265d4fd454cfea491f50324d75106e961a257e
Camille-Durand <camillefanguin@gmail.com>
2023-12-14 15:20:39 +0100

    components

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,40 @@
 <template>
-  <div>
-    <le-header></le-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-  </div>
-</template>
-
-<script>
-import LeHeader from './components/LeHeader.vue';
-export default {
-    components: {
-        'le-header': LeHeader
+    <div>
+      <le-header></le-header>
+      <badge-list></badge-list>
+      <user-info
+        :full-name="activeUser.name"
+        :info-text="activeUser.description"
+        :role="activeUser.role"
+      ></user-info>
+    </div>
+  </template>
+  
+  <script>
+  import LeHeader from './components/LeHeader.vue';
+  import BadgeList from './components/BadgeList.vue';
+  import UserInfo from './components/UserInfo.vue';
+  
+  export default {
+    components:{
+      'le-header':LeHeader,
+      'badge-list':BadgeList,
+      'user-info':UserInfo
     },
-  data() {
-    return {
-      activeUser: {
-        name: 'LOL',
-        description: 'admin du site',
-        role: 'premium',
-      },
-    };
-  },
-};
-</script>
+    data() {
+      return {
+        activeUser: {
+          name: 'LOL',
+          description: 'admin du site',
+          role: 'premium',
+        },
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  html{
+    font-family:impact;
+  }
+  </style>
